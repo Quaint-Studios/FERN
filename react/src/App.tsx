099f00a99042fc963639df821f882572dfeb7fa0
QuaintShanty <quaint.shanty@gmail.com>
2019-12-18 01:44:35 -0500

    Utilized the hooks.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,27 +1,31 @@
-import * as React from 'react';
+import React from 'react';
 
 import logo from './logo.svg';
 
 import './App.css';
 
+import AppContexts from './components/contexts/AppContexts';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContexts>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </AppContexts>
   );
 }
 
